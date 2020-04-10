08bcffc30e0ce132d9377ed4e81bd6b7769c248c
saltemose <sean@seanaltemose.com>
2020-04-09 21:45:23 -0400

    update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ require("channels")
 
 //= require jquery
 //= require rails-ujs
-
+//= require_tree .
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
