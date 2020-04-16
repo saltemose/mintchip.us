4a5a9fcb714ab51a86174ff58407ea5a1ee13e65
saltemose <sean@seanaltemose.com>
2020-04-15 23:02:19 -0400

    fixed authenticity problem

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 //= require jquery
 //= require rails-ujs
 //= require_tree .
+//= require jquery_ujs
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
